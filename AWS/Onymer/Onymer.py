5637881d25c1d112598559de98724c842055ac35
Adam Leclerc <Aleclerc@novetta.com>
2017-06-05 22:26:00 -0400

    removed junk that was duplicated in readme
    moved all this to the README

--- AWS/Onymer/Onymer.py
+++ AWS/Onymer/Onymer.py
@@ -1,41 +1,4 @@
 #!/usr/bin/env python
-'''
-   Onymer - Lambda Function - V 4.0
---------------------------------------------------------------------------------
-Purpose:
-    This will rename all EBS volumes, network interfaces, snapshots, and AMIs
-    in the AWS 'region = ' to:
-        - EBS volumes:  [ instance-name-tag ]-/mount/point
-        - Interfaces: instance-name-tag
-        - Snapshots: [ instance-name-tag ]-/mount/point
-        - AMIs: AMI-Name value (required to make an image, kinda redundant?)
-    Also renames all unattached entities to "- UNATTACHED -previousName" or just
-    - UNATTACHED - if no previous name exists
-Usage:
-   - Run manually in Lambda or (recommended) on a triggered cron
-   (suggested hourly or sooner)
-   - Update region and UnattachedLabel vars to suit your setup
-   - Lambda Configuration:
-        > runtime : python 2.7
-        > handler : lambda_function.lambda_handler
-        > role : (create a role with TODO/TODO )
-        > description : Auto-names all EBS volumes and EC2 Network interfaces
-        Advanced Configuration:
-            > Memory : 128 MB
-            > Timeout : 1 min (should be enough for 1000-1500 checked objects)
-            > DQL Resource: None
-            > VPC : No VPC (not needed really...)
-            > Enable active tracing : unchecked (also no really needed)
-            > KMS key : default
-
-Releases:
-    v4.0 - Added auto-name Snapshots and AMIs to this script
-    v3.0 - Added Auto-name all EC2 network interfaces to the script
-    v2.0 - Auto-rename all EBS volumes, now includes unattached volumes
-    v1.0 - Auto-name all EBS volumes
-
-p.s. - comment out (or uncomment) print lines to alter logging output
-'''
 import json, boto3, logging, time, datetime
 # Program meta -----------------------------------------------------------------
 vers = "4.0"
