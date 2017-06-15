f4cf220ad5535abc157397ddd411181e5f5f7b3b
Adam <aleclerc@localhost.localdomain>
2017-06-15 16:10:20 -0400

    function to get name of instances streamlined

--- AWS/AutoOrc/AutoOrc.py
+++ AWS/AutoOrc/AutoOrc.py
@@ -20,7 +20,19 @@ logger.setLevel(logging.WARNING)
 ls = " - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 # define the connection, replace region if your instances aren't in this region
 region = 'us-east-1'
-ec2 = boto3.resource('ec2', region_name=region) 
+ec2 = boto3.resource('ec2', region_name=region)
+UnNamedLabel = "no name?"
+# function to return name of instances for an instance ID
+def get_instance_name(ec2id):
+    EC2Instance = ec2.Instance(ec2id)
+    InstanceName = ''
+    if EC2Instance.tags is not None:
+        for tags in EC2Instance.tags:
+            if tags["Key"] == 'Name':
+                InstanceName = tags["Value"]
+    else:
+        InstanceName = UnNamedLabel
+    return InstanceName
 # main function, that lambda 'calls'
 def lambda_handler(event, context):
     # set variable to figure out what day of the week it is
@@ -29,7 +41,7 @@ def lambda_handler(event, context):
     timer = time.strftime("%H:%M")
     print ls
     print "[ Orc routine start time : " + timer + " ]"
-    # set base filters for running/stopped instances, and matching orc tags    
+    # set base filters for running/stopped instances, and matching orc tags
     filter_running = [
         {'Name': 'instance-state-name','Values': ['running']},
         {'Name': 'tag:autoOrc-down','Values': [timer]}
@@ -39,28 +51,22 @@ def lambda_handler(event, context):
         {'Name': 'tag:autoOrc-up','Values': [timer]}
         ]
     # collect all running instances and filter for orc down tag
-    orc_instances = ec2.instances.filter(Filters=filter_running)   
+    orc_instances = ec2.instances.filter(Filters=filter_running)
     for instance in orc_instances:
-        name = "no name?"
-        for tag in instance.tags:
-            if 'Name'in tag['Key']:
-                name = tag['Value']
+        name = get_instance_name(instance.id)
         # Print the instances stopping for logging purposes
         print "Shutting down instance: "
         print(instance.id) + " [ Name : " + name + " ] "
         instance.stop()
     # determine all stopped instances and filter for the  orc up tag
-    orc_instances_up = ec2.instances.filter(Filters=filter_stopped) 
-    # check to make sure we're only starting stuff on weekdays  
+    orc_instances_up = ec2.instances.filter(Filters=filter_stopped)
+    # check to make sure we're only starting stuff on weekdays
     if d.isoweekday() in range(1, 6):
         for instance in orc_instances_up:
-            name = "no name?"
-            for tag in instance.tags:
-                if 'Name'in tag['Key']:
-                    name = tag['Value']
+            name = get_instance_name(instance.id)
             # Print the instances starting for logging purposes
             print "---> Starting instance: "
             print(instance.id) + " [ Name : " + name + " ] "
-            instance.start()    
-    print "[ Orc routine finished ]"        
+            instance.start()
+    print "[ Orc routine finished ]"
     print ls
