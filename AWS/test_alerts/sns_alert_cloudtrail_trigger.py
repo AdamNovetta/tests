bfe541722c6dde87d81744663229fd13f6df7703
Adam Leclerc <Aleclerc@novetta.com>
2017-07-12 12:06:40 -0400

    Update sns_alert_cloudtrail_trigger.py

--- AWS/test_alerts/sns_alert_cloudtrail_trigger.py
+++ AWS/test_alerts/sns_alert_cloudtrail_trigger.py
@@ -2,6 +2,7 @@
 # tools needed
 import json, boto3, logging, time, datetime
 from pprint import pprint
+from time import mktime
 # Program meta -----------------------------------------------------------------
 vers = "1.0"
 prog_name = "sns_alert_cloudtrail_trigger"
@@ -10,11 +11,18 @@ prog_name = "sns_alert_cloudtrail_trigger"
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 #  -----------------------------------------------------------------------------
+# Class to render JSON outputs (with dates) successfully
+class Render(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return int(mktime(obj.timetuple()))
+
+        return json.JSONEncoder.default(self, obj)
+#  -----------------------------------------------------------------------------
 # make connections to services
 ec2_region_name = "us-east-1"
 s3_client = boto3.client('s3')
-s3_buckets = s3_client.list_buckets()
-s3_object = boto3.resource('s3')
 # SNS
 sns_client = boto3.client('sns')
 My_AWS_ID = boto3.client('sts').get_caller_identity().get('Account')
@@ -24,5 +32,5 @@ def lambda_handler(event, context):
     print "SNS ARN: " + sns_arn
     print "EVENT: " + str(event)
     print "\nCONTEXT: " + str(context)
-    sns_message =  "Cloudtrail log \n [ " + str(event) + " ]\n"
+    sns_message =  "Cloudtrail log \n [ " + json.dumps(event, cls=Render) + " ]\n"
     sns_client.publish(TopicArn=sns_arn, Message=sns_message, Subject='Alert! Cloudtrail triggered')
