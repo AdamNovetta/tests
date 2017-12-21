69b3c47a26b0e1f364595c4b4811fd7c28a5c9ad
Adam Leclerc <Aleclerc@novetta.com>
2017-12-21 17:09:14 -0500

    Update IAM_userID_report.py

--- AWS/test_reports/IAM_userID_report.py
+++ AWS/test_reports/IAM_userID_report.py
@@ -6,11 +6,11 @@ import logging
 import time
 import datetime
 from time import mktime
-# PMeta
+# Meta
 vers = "1.0"
 ProgramName = "IAM_userID_reports"
 line = "______________________________________________________________________"
-#  -----------------------------------------------------------------------------
+
 # output logging for INFO, to see full output in cloudwatch, default to warning
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -20,28 +20,34 @@ IAM_client = boto3.client('iam')
 
 
 def lambda_handler(event, context):
-
-    class Render(json.JSONEncoder):
-
-        def default(self, obj):
-            if isinstance(obj, datetime.datetime):
-                return int(mktime(obj.timetuple()))
-            return json.JSONEncoder.default(self, obj)
-
+    heading = ['Name', 'ID']
+    names = []
+    IDs = []
+    column = 0
     AllIAMUsers = IAM_client.get_paginator('list_users')
 
+    def length_check(check, col=column):
+        if len(check) > col:
+            col = len(check)
+
+        return(col)
+
     for users in AllIAMUsers.paginate():
-        # look at all user data by uncommenting this line
-        # print(json.dumps(users, cls=Render))
-        userCol = idCol = 0
-        userList = {}
-        print(line + "\nIAM Username    |    IAM User ID\n" + line)
         for user in users['Users']:
-            if len(user['UserName']) > userCol:
-                userCol = len(user['UserName'])
-            if len(user['UserId']) > idCol:
-                idCol = len(user['UserId'])
-            userList = user['UserName']: user['UserId']
-        for user in userList:
-            print(user)
+            n = user['UserName']
+            i = user['UserId']
+            names.append(n)
+            IDs.append(i)
+            items = [n, i]
+            length = max(map(length_check, items))
+            if length > column:
+                column = length
+
+    output = [heading] + list(zip(names, IDs))
+    print("\n")
+    for i, d in enumerate(output):
+        line = ' | '.join(str(x).ljust(column) for x in d)
         print(line)
+        if i == 0:
+            print('-' * len(line))
+    print("\n")
