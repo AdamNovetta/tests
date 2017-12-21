36ab1117c7f65f252b8372854a0ee3c7f8796710
AdamNovetta <aleclerc@novetta.com>
2017-12-21 17:22:59 -0500

    updating report

--- AWS/test_reports/IAM_roleID_report.py
+++ AWS/test_reports/IAM_roleID_report.py
@@ -20,23 +20,36 @@ IAM_client = boto3.client('iam')
 
 
 def lambda_handler(event, context):
-
-    class Render(json.JSONEncoder):
-
-        def default(self, obj):
-            if isinstance(obj, datetime.datetime):
-                return int(mktime(obj.timetuple()))
-
-            return json.JSONEncoder.default(self, obj)
-
+    heading = ['Role Name', 'Role ID']
+    names = []
+    IDs = []
+    column = 0
     AllIAMRoles = IAM_client.get_paginator('list_roles')
+
+    def length_check(check, col=column):
+        if len(check) > col:
+            col = len(check)
+
+        return(col)
 
     for roles in AllIAMRoles.paginate():
         # look at all user data by uncommenting this line
         # print(json.dumps(roles, cls=Render))
-
-        print line + "\nIAM Role Name    |    IAM Role ID\n" + line
         for role in roles['Roles']:
-            print role['RoleName'] + " | " + role['RoleId']
+            n = role['RoleName']
+            i = role['RoleId']
+            names.append(n)
+            IDs.append(i)
+            items = [n, i]
+            length = max(map(length_check, items))
+            if length > column:
+                column = length
 
+    output = [heading] + list(zip(names, IDs))
+    print("\n")
+    for i, d in enumerate(output):
+        line = ' | '.join(str(x).ljust(column) for x in d)
         print(line)
+        if i == 0:
+            print('-' * len(line))
+    print("\n")
