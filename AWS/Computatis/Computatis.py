82a9d949f693887b6b72e4c9a0b4517e6eb70652
AdamNovetta <aleclerc@novetta.com>
2017-12-22 18:29:43 -0500

    more checking and some function...

--- AWS/Computatis/Computatis.py
+++ AWS/Computatis/Computatis.py
@@ -21,15 +21,20 @@ giturl = 'https://api.github.com/repos/'
 gitraw = 'https://raw.githubusercontent.com/'
 targetRepo = '/AWS-API-TOOLS'
 gitContent = '/contents'
-targetDir = '/scripts/'
+scripts = '/scripts/'
+perms = '/permissions/'
 rawBranch = '/master'
 
 
-# Filetype of Lambda scripts
+# Filetype and runtime info of Lambda scripts
 ext = '.py'
+runtime = 'python3.6'
+handler = 'lambda_function.lambda_handler'
+IAMext = '.IAM'
 
 # Define boto3 connections/variables
 lambda_client = boto3.client('lambda')
+IAM_client = boto3.client('iam')
 
 
 # Get AWS Lambda functions currently in this AWS account
@@ -39,6 +44,7 @@ def get_available_functions():
     for function in AvailableFunctions:
         if function['FunctionName'] != ProgramName:
             AFList.append(function['FunctionName'])
+
     return(AFList)
 
 
@@ -72,7 +78,8 @@ def get_function_content(l):
 
 # Get AWS Lambda functions with .py extentions on the designated repo above
 def get_functions_masters():
-    repo = requests.get(giturl + user + targetRepo + gitContent + targetDir)
+    string = giturl + user + targetRepo + gitContent + scripts
+    repo = requests.get(string)
     GFList = []
     if(repo.ok):
         repoItem = json.loads(repo.content)
@@ -86,39 +93,83 @@ def get_functions_masters():
     return(GFList)
 
 
-def get_git_contents(fname):
-    string = gitraw + user + targetRepo + rawBranch + targetDir + fname + ext
+def get_git_contents(fname, objs):
+    if objs == scripts:
+        string = gitraw + user + targetRepo + rawBranch + objs + fname + ext
+    if objs == perms:
+        string = gitraw + user + targetRepo + rawBranch + objs + fname + IAMext
     repo = requests.get(string)
     if(repo.ok):
         file_contents = repo.content
     return(file_contents)
 
 
+def get_IAM_role_names():
+    output = []
+    roles = IAM_client.list_roles()
+    for role in roles['Roles']:
+        output.append(role['RoleName'])
+    return(output)
+
+
+def get_IAM_role_permissions(rname):
+    output = IAM_client.list_role_policies(RoleName=rname)
+    return(output['PolicyNames'])
+
+
+def sync_git_to_aws(fname, code):
+    output = lambda_client.update_function_code(
+                                                    FunctionName='string',
+                                                    ZipFile=code,
+                                                    # Publish=True
+                                                    DryRun=True
+                                                )
+    return(output)
+
+
+def create_lambda_function(fname, code):
+    return(print("   --> function to create is not implemented yet"))
+
+
 def lambda_handler(event, context):
     LambdaFunctions = get_available_functions()
+    IAMRoles = get_IAM_role_names()
     MasterIndex = get_functions_masters()
     AWS_Lambdas = {}
     Git_Functions = {}
 
     for i in LambdaFunctions:
+        rolename = "lambda-"+i
         Loc = get_function_url(i)
         content = get_function_content(Loc)
-        AWS_Lambdas[i] = content
+        AWS_Lambdas[i] = {}
+        AWS_Lambdas[i]['Code'] = content
+        if rolename in IAMRoles:
+            print(get_IAM_role_permissions(rolename))
 
     for i in MasterIndex:
-        GitContents = get_git_contents(i)
 
-        Git_Functions[i] = GitContents
+        GitContents = get_git_contents(i, scripts)
+        IAMContents = get_git_contents(i, perms)
+        # p = IAMContents['Statement']['Sid']
+        Git_Functions[i] = {}
+        Git_Functions[i]['Code'] = GitContents
+        Git_Functions[i]['IAM'] = IAMContents
+
     for x in Git_Functions:
+        policy = json.loads(Git_Functions[x]['IAM'].decode())
+        print(policy['Statement'][0]['Sid'])
         if x in AWS_Lambdas:
-            if AWS_Lambdas[x] == Git_Functions[x]:
-                print(x + " is on the AWS account and the Code matches!")
+            if AWS_Lambdas[x]['Code'] == Git_Functions[x]['Code']:
+                print(x + " already on AWS account and code matches!")
             else:
                 print("XXX CODE NOT THE SAME FOR : " + x + " XXX")
-                print("Code on git: \n" + str(Git_Functions[x].decode()))
-                print("Code on AWS: \n" + str(AWS_Lambdas[x].decode()))
+                print("Git code:\n" + str(Git_Functions[x]['Code'].decode()))
+                print("AWS code:\n" + str(AWS_Lambdas[x]['Code'].decode()))
+                sync_git_to_aws(x, Git_Functions[x]['Code'])
         if x not in AWS_Lambdas:
             print(" - " + x + " is missing from AWS Lambda list!")
+            create_lambda_function(x, Git_Functions[x]['Code'])
 
     # TODO
     # check/diff local functions vs master index (and versions):
