f226d52eb2c85d958dc8fc7fda2a4f7e9a75d1e9
Adam Leclerc <Aleclerc@novetta.com>
2018-08-23 14:14:32 -0700

    Update lambda_function.py

--- AWS/seneschal/lambda_function.py
+++ AWS/seneschal/lambda_function.py
@@ -1,1 +1,21 @@
 
+#!/usr/bin/env python3
+import json
+import boto3
+import logged
+import time
+import datetime
+
+##########
+## Meta ##
+##########
+Vers = "1.0"
+ProgramName = "Seneschal AAAP"
+Desc = "AWS Admin Automation Program"
+##########
+##########
+
+    
+def lambda_handler(event, context):
+    log = logged.log_data(ProgramName,Vers)
+    log.starting("autotasks")
