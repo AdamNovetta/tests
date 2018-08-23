98ceaefab1aed6f5b262f0a8e5b272a9a1cdda4f
Adam Leclerc <Aleclerc@novetta.com>
2018-08-23 16:27:25 -0400

    log.py first draft
    first draft

---
+++ AWS/seneschal/log.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+import json
+import boto3
+import logging
+import time
+import datetime
+
+# Set below to False to disable logging output
+ENABLE_LOGGING = True
+
+# Iteration counter for naming passes / debugging
+class counter:
+        def __init__(self):
+                self.number = 0
+                self.total = 0
+
+        def add(self):
+                self.number += 1
+                self.total += 1
+
+        def reset(self):
+                self.number = 0
+
+
+# logging output class
+class log_data:
+
+    # create blank logging-item object with counter
+    def __init__(self):
+        self.state = "starting"
+        self.proc = ''
+        self.data = ''
+        self.count = counter()
+
+    def starting(self, process):
+        self.count.reset()
+        self.state = "starting"
+        self.proc = process
+        print_log(self)
+
+    def process(self, p, d, s):
+        self.state = s
+        self.proc = p
+        self.data = d
+        self.count.add()
+        print_log(self)
+
+    def ending(self, process):
+        self.state = "ending"
+        self.proc = process
+        print_log(self)
+
+    def finished(self):
+        self.state = "finished"
+        print_log(self)
+
+    # log print output
+    def __str__(self):
+        ############################
+        ## Output messaging vars  ##
+        ############################
+        output = d = more_data = ''
+        err = " X ERROR X "
+        suc = "+ - "
+        header = "[ " + ProgramName + " - " + Vers + " | "
+        tailed = " process @ " + d + " ]"
+        footer = "[ <!> ____ Processed: "
+        counted = " objects during "
+        footend = " tasks _____ <!> ]\n"
+        ending = "\n[ <O> ____ Completed! Processed : "
+        totaled = " total objects in "
+        endend = " run _____ <O> ]\n"
+        dash = " - "
+        sp = " "
+        #############################
+        #############################
+
+        logger = logging.getLogger()
+        # set below to DEBUG or INFO to see more errors in event log/console
+        logger.setLevel(logging.WARNING)
+
+        count = str(self.count.number)
+        total = str(self.count.total)
+
+        if self.data:
+            more_data = str(self.data)
+
+        d = str(datetime.datetime.now())
+
+        if "0" in self.state:
+            output = err + self.proc + dash + more_data
+        if "1" in self.state:
+            output = suc + self.proc + dash + more_data
+        if "starting" in self.state:
+            output =  header + self.state + sp + self.proc + tailed
+        if "ending" in self.state:
+            output = footer + count + counted + self.proc + footer
+        if "finished" in self.state:
+            output = ending + total + totaled + ProgramName + endend
+
+        return(output)
+
+# Check if logging on/off
+def print_log(logger_name):
+    if ENABLE_LOGGING:
+        print(logger_name)
